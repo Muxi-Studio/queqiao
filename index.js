83292c13ad5fad9ebe13a3345701d8a7cca4c46b
zxc0328 <120797887@qq.com>
2017-03-20 23:24:49 +0800

    Update test code for mongodb

--- index.js
+++ index.js
@@ -1,8 +1,45 @@
-const Koa = require('koa');
-const app = new Koa();
+'use strict';
+const koa = require('koa');
+const mongo = require('koa-mongo');
+const Model = require('./model');
+const Router = require('koa-router');
 
-app.use(ctx => {
-  ctx.body = 'Hello World';
+const app = new koa();
+const router = new Router();
+
+app.use(mongo());
+app.use(router.routes());
+app.use((ctx, next) => {
+  ctx.database = ctx.mongo.db("test");
+  next();
 });
 
-app.listen(3000);
+router
+  .get('/', async (ctx, next) => {
+  	const user = new Model(ctx.mongo.db("test"), "User");
+    const result = await user.insertOne({username: "hahahha"});
+    ctx.body = result;
+  })
+  .get('/users/:id', async (ctx, next) => {
+  	const id = ctx.params.id;
+  	const user = new Model(ctx.mongo.db("test"), "User");
+    const result = await user.findOneById(id);
+    ctx.body = result;
+  })
+  .post('/users', function (ctx, next) {
+    // ...
+  })
+  .put('/users/:id', function (ctx, next) {
+    // ...
+  })
+  .del('/users/:id', function (ctx, next) {
+    // ...
+  })
+  .all('/users/:id', function (ctx, next) {
+    // ...
+  });
+
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
